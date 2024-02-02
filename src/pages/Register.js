e0a68dcdb2c04496281e13d3d72496eed20b6989
AQR88 <timahaua@gmail.com>
2024-02-02 12:18:02 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,10 +3,9 @@ import { RegisterForm } from '../components/RegisterForm/RegisterForm';
 
 export default function Register() {
   return (
-    <div>
+    <>
       <title>Registration</title>
-
       <RegisterForm />
-    </div>
+    </>
   );
 }
